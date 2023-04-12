4ebd909c078915b9311e696aa93b8b2a79eb855f
TheApplePieGod <evant.002@gmail.com>
2023-04-12 17:45:40 -0400

    More visualization

--- client/visualizer.py
+++ client/visualizer.py
@@ -5,8 +5,8 @@ from controller import Controller
 
 
 # Style constants
-HEALTH_CELL_WIDTH = 2
-HEALTHBAR_SPACING = 4
+BAR_CELL_WIDTH = 2
+BOT_SPACING = 5
 
 
 # Color pair constants
@@ -63,22 +63,59 @@ class Visualizer:
             state: Game state as defined in server/server.py
         """
         # rectangle(self.scr, 5, 5, 10, 10)
-        self._draw_team((5, 5), state["bots"])
-        self._draw_team((5, 20), state["op_bots"])
+        self._draw_team(
+            (5, 5),
+            state["bots"],
+            #state["actions"],
+            #state["op_actions"]
+        )
+        self._draw_team(
+            (5, 20),
+            state["op_bots"],
+            #state["op_actions"],
+            #state["actions"]
+        )
+
         self.scr.refresh()
+
+    def _get_shield_health(self, actions, opp_actions, bot_idx):
+        # Ensure bot actually shielded
+        if actions[bot_idx]["type"] != "shield":
+            return None
+
+        # If so, determine health of shield
+        health = Controller.SHIELD_HEALTH
+        for action in opp_actions:
+            if action["type"] == "launch" and action["target"] == bot_idx:
+                health -= action["strength"]
+
+        return max(health, 0)
 
     def _draw_multiline_text(self, pos, text, args=0):
         for i, line in enumerate(text.split('\n')):
             if line:
                 self.scr.addstr(pos[1] + i, pos[0], line, args)
 
-    def _draw_team(self, pos, bots):
+    def _draw_team(self, pos, bots): #, actions, opp_actions):
         start_x = pos[0]
         for i, bot in enumerate(bots):
-            end_x = self._draw_health(
-                (start_x, pos[1]), bot[0]
-            ) + HEALTHBAR_SPACING
+            # Health bar
+            end_x = self._draw_bar(
+                (start_x, pos[1]),
+                bot[0], Controller.INITIAL_HEALTH,
+                HEALTH_FILLED, HEALTH_EMPTY
+            ) + BOT_SPACING
 
+            # Shield bar
+            """
+            shield_health = self._get_shield_health(
+                actions, opp_actions, i
+            )
+            if shield_health is not None:
+                pass
+            """
+
+            # Render bot
             middle = start_x + (end_x - start_x) // 2
             self._draw_multiline_text(
                 (
@@ -90,33 +127,36 @@ class Visualizer:
 
             start_x = end_x
 
-    def _draw_health(self, pos, health):
+    def _draw_bar(self, pos, cur, max, color_filled, color_empty):
         """
-        Render a health bar
+        Render a progress bar
 
         Args:
             screen: Curses screen context
             pos (tuple(int, int)): x and y pos of left side of rendered bar
-            health (int): Current helth
+            cur (int): Current value
+            max (int): Maximum value
+            color_filled (int): Color pair to use for the filled portion
+            color_empty (int): Color pair to use for the empty portion
 
         Returns (int):
             x coordinate directly after the end of the bar
         """
-        filled = health * HEALTH_CELL_WIDTH
+        filled = cur * BAR_CELL_WIDTH
         self.scr.addstr(
             pos[1], pos[0],
             " " * filled,
-            curses.color_pair(HEALTH_FILLED)
+            curses.color_pair(color_filled)
         )
 
-        empty = Controller.INITIAL_HEALTH - health
+        empty = max - cur
         self.scr.addstr(
             pos[1], pos[0] + filled,
-            " " * empty * HEALTH_CELL_WIDTH,
-            curses.color_pair(HEALTH_EMPTY)
+            " " * empty * BAR_CELL_WIDTH,
+            curses.color_pair(color_empty)
         )
 
-        return pos[0] + filled + empty * HEALTH_CELL_WIDTH
+        return pos[0] + filled + empty * BAR_CELL_WIDTH
 
     def _init_colors(self):
         """
